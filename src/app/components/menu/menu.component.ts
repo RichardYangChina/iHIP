13c7cb97e17c19c4347440c2f7e8dd197b4a60f5
RichardLinYang <yanglin@yitongmed.com>
2018-11-15 14:13:20 +0800

    Ui change

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -3,7 +3,8 @@ import { MenuService } from 'src/app/services/menu.service';
 
 @Component({
   selector: 'app-menu',
-  templateUrl: './menu.component.html'
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
   menuData = [];
